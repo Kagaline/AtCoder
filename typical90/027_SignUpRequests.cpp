85dc4ffd3d2b1dcf983713b2351ef2948f30b2f5
Kagaline <wackiyoriginal@gmail.com>
2023-04-20 21:12:25 +0900

    solved 027

---
+++ typical90/027_SignUpRequests.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>    // ハッシュの役割をする
+
+int main(void)
+{
+
+    int Num_inputs;
+    std::cin >> Num_inputs;
+
+    // 登録名->すでに登録しているか　を調べるためのハッシュを用意
+    std::unordered_map<std::string, int> exists_name;
+
+    //
+    std::string enroll_name;
+    for (int i = 0; i < Num_inputs; i++) {
+        std::cin >> enroll_name;
+        if (exists_name[enroll_name] == 0) {
+            exists_name[enroll_name] = 1;
+            std::cout << i + 1 << std::endl;
+        }
+    }
+
+    return 0;
+}
