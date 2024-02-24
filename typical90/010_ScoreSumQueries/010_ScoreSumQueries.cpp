1e12d369848da5641f80304afb13a930927d6544
Kagaline <wackiyoriginal@gmail.com>
2024-02-24 17:45:17 +0900

    update: changed to use push_back method to show logic clearly.

--- typical90/010_ScoreSumQueries/010_ScoreSumQueries.cpp
+++ typical90/010_ScoreSumQueries/010_ScoreSumQueries.cpp
@@ -9,33 +9,45 @@ int main(void)
   int Num_students;
   std::cin >> Num_students;
 
-  // それぞれのクラスについて学籍番号までの点数の和
-  std::vector<int> sum_class_1(Num_students + 1);
-  std::vector<int> sum_class_2(Num_students + 1);
+  // それぞれのクラスについて学籍番号までの点数の累積和
+  std::vector<int> sum_class_1 = {0};
+  std::vector<int> sum_class_2 = {0};
 
-  // 学生番号は1からなので0番には0を入れる
-  sum_class_1.at(0) = 0;
-  sum_class_2.at(0) = 0;
+  // 累積和を計算する
+  for (int i = 0; i < Num_students; i++) {
 
-  int class_num;
-  int point;
-  for (int i = 1; i <= Num_students; i++) {
-    std::cin >> class_num >> point;
-    if (class_num == 1) {
-      sum_class_1.at(i) = sum_class_1.at(i - 1) + point;
-      sum_class_2.at(i) = sum_class_2.at(i - 1) + 0;
-    } else {
-      sum_class_1.at(i) = sum_class_1.at(i - 1) + 0;
-      sum_class_2.at(i) = sum_class_2.at(i - 1) + point;
+    // クラスが1であることを示す
+    const int class_one = 1;
+
+    // クラスと点数を読み込む
+    int class_name;
+    int score;
+    std::cin >> class_name >> score;
+
+    if (class_name == class_one) {    // クラス1の時
+      sum_class_1.push_back(sum_class_1.back() + score);
+      sum_class_2.push_back(sum_class_2.back());
+    } else {    // クラス2の時
+      sum_class_1.push_back(sum_class_1.back());
+      sum_class_2.push_back(sum_class_2.back() + score);
     }
   }
 
-  int Num_queries, start, end;
+  // クエリに応じて結果を出力する
+  int Num_queries;
   std::cin >> Num_queries;
+
   for (int i = 0; i < Num_queries; i++) {
+    int start, end;
     std::cin >> start >> end;
-    std::cout << sum_class_1.at(end) - sum_class_1.at(start - 1) << " "
-              << sum_class_2.at(end) - sum_class_2.at(start - 1) << std::endl;
+
+    // 指定区間の累積和を計算する
+    const int class_1_total_score = sum_class_1.at(end) - sum_class_1.at(start - 1);
+    const int class_2_total_score = sum_class_2.at(end) - sum_class_2.at(start - 1);
+
+    // 結果を出力する
+    std::cout << class_1_total_score << " " << class_2_total_score << std::endl;
   }
+
   return 0;
 }
